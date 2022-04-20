33787fbb09d9125ede19caf00e9040de9b02bb49
Jianfeng Wang <me@jeffreywang.cn>
2022-04-20 10:50:21 +0800

    [update] trim human readable

--- utils.js
+++ utils.js
@@ -1,18 +1,19 @@
 // from https://stackoverflow.com/questions/8211744/convert-time-interval-given-in-seconds-into-more-human-readable-form
 function forHumans ( seconds ) {
     var levels = [
-        [Math.floor(seconds / 31536000), 'Y'],
-        [Math.floor((seconds % 31536000) / 86400), 'D'],
-        [Math.floor(((seconds % 31536000) % 86400) / 3600), 'H'],
-        [Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), 'M'],
-        [(((seconds % 31536000) % 86400) % 3600) % 60, 'S'],
+        [Math.floor(seconds / 31536000), 'years'],
+        [Math.floor((seconds % 31536000) / 86400), 'days'],
+        [Math.floor(((seconds % 31536000) % 86400) / 3600), 'hours'],
+        [Math.floor((((seconds % 31536000) % 86400) % 3600) / 60), 'minutes'],
+        [(((seconds % 31536000) % 86400) % 3600) % 60, 'seconds'],
     ];
     var returntext = '';
 
-    for (var i = 0, max = levels.length; i < max; i++) {
+    for (var i = 0, j = 0, flag = false, max = levels.length; i < max && j < 1; i++) {
+        if (flag) j++;
         if ( levels[i][0] === 0 ) continue;
-        // returntext += ' ' + levels[i][0] + ' ' + (levels[i][0] === 1 ? levels[i][1].substr(0, levels[i][1].length-1): levels[i][1]);
-        returntext += ' ' + levels[i][0] + levels[i][1];
+        returntext += ' ' + levels[i][0] + ' ' + (levels[i][0] === 1 ? levels[i][1].substr(0, levels[i][1].length-1): levels[i][1]);
+        flag = true;
     };
     return returntext.trim();
 }
